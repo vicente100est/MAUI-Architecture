7ccd7371c290fa35f1b4f3339f482851dc08d69d
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2023-01-11 15:56:16 -0600

    Arch MAUI

---
+++ Arquitectura/Guidinator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arquitectura
+{
+    public class Guidinator : IMarkupExtension<string>
+    {
+        public string ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ProvideValue(serviceProvider);
+        }
+    }
+}
---
+++ Arquitectura/Platforms/Android/GeolocationDevice.cs
@@ -0,0 +1,21 @@
+﻿using Android.Content;
+using Android.Locations;
+using Android.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arquitectura;
+public partial class GeolocationDevice
+{
+    public partial string GetName()
+    {
+        var lm = Android.App.Application.Context
+            .GetSystemService(Context.LocationService)
+            .JavaCast<LocationManager>();
+
+        return lm.GnssHardwareModelName;
+    }
+}
